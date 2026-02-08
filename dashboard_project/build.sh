a1d759305ec3c1fcd5e791a3e05cbe93f02ea750
Daksh Dagar <80425622+DakshDagar@users.noreply.github.com>
2026-02-08 07:53:06 -0500

    Add React build to Render deployment process

--- dashboard_project/build.sh
+++ dashboard_project/build.sh
@@ -2,6 +2,13 @@
 # exit on error
 set -o errexit
 
+# Build React frontend
+cd ..
+npm install
+npm run build
+cd dashboard_project
+
+# Install Python dependencies and setup Django
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
